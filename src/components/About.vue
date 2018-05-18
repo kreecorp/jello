4dfb21ae7b84543fd3eb7a1ef636cabe59799e16
Casprine Assempah <casprine.001@gmail.com>
2018-05-18 03:16:34 +0000

    content added

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,8 +1,21 @@
 <template>
   <main>
+
     <h1> 
       Beautifully crafted frontend components
     </h1>
+
+    <div class="about center">
+        <p>
+            Premuim designed frontend components with no configuration. <br>
+            Download what you need without any bloat of unneeded css
+        </p>
+    </div>
+
+      <div class="get-started center">
+                <button class="stats"> use cases </button>
+                <button class="stats"> components</button>
+      </div>
   </main>             
 </template>
 
