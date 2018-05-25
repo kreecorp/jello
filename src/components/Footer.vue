2e47327c5943f9daaa10693ba856385a2d0191cf
Casprine Assempah <casprine.001@gmail.com>
2018-05-25 01:20:40 +0000

    finished footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,21 @@
 <template>
   <footer>
+        <div class="wrap center">
+    <div class="about-us">
+            <div class="name"> Jello </div>
+            <div class="license">Released under MIT License</div>
+    </div>
+
+    <div class="links">
+        <a href="https://twitter.com/_getKre" target="_blank"> <ion-icon name="logo-twitter"></ion-icon></a>
+        <a href="https://github.com/KreeCorp/Jello"  target="_blank"><ion-icon name="logo-github"></ion-icon></a>
+    </div>
+
+
+    <div class="copyright">
+    Copyright   <span>  © </span>  {{ new Date().getFullYear() }}
+    </div>
+    </div>
   </footer>
 </template>
 
